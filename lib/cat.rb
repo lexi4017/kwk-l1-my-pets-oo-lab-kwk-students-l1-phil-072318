8b80b8f88505461c77a02b437094006a6b095aaf
Alexsia Light <elightl729@comcast.net>
2018-07-26 20:31:22 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,22 @@
 class Cat
-  # code goes here
+ def initialize(name, mood)
+   @name = name
+   @mood = name
+ end
+ 
+  def return_name
+    @name
+  end
+  def return_mood
+    @mood
+  end
+  def reset_mood=(new_mood)
+    @mood = new_mood
+  end
+  
   
 end
+
+vinny = Cat.new("Vinny", "Mad")
+
+puts vinny.return_name
